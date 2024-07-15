d67899826158f5a333556b2ecf288aa625f2a2ab
Vanshika <guptavanshika220@gmail.com>
2024-07-15 16:28:11 +0530

    chnages

--- functions/decrementcounter.js
+++ functions/decrementcounter.js
@@ -1,7 +1,6 @@
 export async function onRequest(context) {
     const { MYKVSTORE } = context.env;
     let counterValue = await MYKVSTORE.get('counter');
-    counterValue = parseInt(counterValue) || 0;
     counterValue -= 1;
     await MYKVSTORE.put('counter', counterValue);
     return new Response(counterValue);
