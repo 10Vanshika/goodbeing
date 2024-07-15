0418136ee2bb7ad8f13f7b34caa4eb44c337e303
Vanshika <guptavanshika220@gmail.com>
2024-07-15 13:14:52 +0530

    Increment counter

--- functions/counterval.js
+++
@@ -1,5 +0,0 @@
-export async function onRequest(context) {
-    const { MYKVSTORE } = context.env;
-    const counterValue = await MYKVSTORE.get('Counter');
-    return new Response(counterValue);
-  }  
