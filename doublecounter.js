4f0efd4eb0bf2d505fa184ada745ccc9a013b6e1
Vanshika <guptavanshika220@gmail.com>
2024-07-15 16:12:08 +0530

    creating more buttons

---
+++ doublecounter.js
@@ -0,0 +1,8 @@
+export async function onRequest(context) {
+    const { MYKVSTORE } = context.env;
+    let counterValue = await MYKVSTORE.get('counter');
+    counterValue = parseInt(counterValue) || 0;
+    counterValue = counterValue*2;
+    await MYKVSTORE.put('counter', counterValue);
+    return new Response(counterValue);
+}
