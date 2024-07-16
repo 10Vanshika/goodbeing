cab4d24305b86ce77569e3f5ef3d499bc1b4f45c
Vanshika <guptavanshika220@gmail.com>
2024-07-16 11:51:26 +0530

    changer to counter value

--- functions/resetfunc.js
+++ functions/resetfunc.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
     const { MYKVSTORE } = context.env;
-    let counterValue = await MYKVSTORE.get('reset');
+    let counterValue = await MYKVSTORE.get('counter');
     return new Response(counterValue);
   }  
