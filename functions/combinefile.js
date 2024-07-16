28653f52c8b3f6f3b2bfab38f5a8cda264febba0
Vanshika <guptavanshika220@gmail.com>
2024-07-16 13:19:34 +0530

    change in code

--- functions/combinefile.js
+++ functions/combinefile.js
@@ -6,15 +6,15 @@ export async function onRequest(context) {
     let counterValue = await MYKVSTORE.get('counter');
     counterValue = parseFloat(counterValue) || 0;
 
-    if (action =='increment') {
+    if (action ==='increment') {
         counterValue += 1;
-    } else if(action = 'decrement'){
+    } else if(action === 'decrement'){
         counterValue -= 1;
         
-    }else if(action =='half'){
+    }else if(action ==='half'){
         counterValue = counterValue/2;
       
-    }else if (action =='double'){
+    }else if (action ==='double'){
         counterValue = counterValue*2;
        
     }
@@ -26,3 +26,4 @@ export async function onRequest(context) {
     await MYKVSTORE.put('counter',counterValue);
     return new Response(counterValue);
 }
+
