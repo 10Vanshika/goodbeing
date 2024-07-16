ff01285a1d7872d92ad53c0e77a4e8db3f84c803
Vanshika <guptavanshika220@gmail.com>
2024-07-16 12:20:08 +0530

    store kv value

--- functions/resetfunc.js
+++ functions/resetfunc.js
@@ -1,5 +1,6 @@
 export async function onRequest(context) {
     const { MYKVSTORE } = context.env;
     let counterValue = 0;
+    await MYKVSTORE.put('counter', counterValue);
     return new Response(counterValue);
   }  
