04cec2ec44dbfd4b0dbd1ad6efc657ca5992358c
Vanshika <guptavanshika220@gmail.com>
2024-07-16 11:58:28 +0530

    change of counter value

--- functions/resetfunc.js
+++ functions/resetfunc.js
@@ -1,5 +1,5 @@
 export async function onRequest(context) {
     const { MYKVSTORE } = context.env;
-    let counterValue = await MYKVSTORE.get('counter');
+    let counterValue = 0;
     return new Response(counterValue);
   }  
