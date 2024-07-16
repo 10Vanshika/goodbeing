e26a70f842b31f7652f19778a97adf1a43f2adee
Vanshika <guptavanshika220@gmail.com>
2024-07-16 15:56:32 +0530

    storing value of commnet in database

--- functions/commentfunc.js
+++ functions/commentfunc.js
@@ -2,5 +2,6 @@ export async function onRequest(context) {
     const { MYKVSTORE } = context.env;
     let counterValue = await MYKVSTORE.get('comment');
     counterValue = parseFloat(counterValue) || 0;
+    await MYKVSTORE.put('comment',counterValue)
     return new Response(counterValue);
   }  
