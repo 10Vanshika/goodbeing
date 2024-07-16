f1a9c8c7b60685d9305cdf1df582c8291f0e95f1
Vanshika <guptavanshika220@gmail.com>
2024-07-16 16:51:18 +0530

    updating the code

--- functions/commentfunc.js
+++ functions/commentfunc.js
@@ -1,7 +1,10 @@
 export async function onRequest(context) {
     const { MYKVSTORE } = context.env;
     const { searchParams } = new URL(context.request.url);
-    
+    const action = searchParams.get('action');
+if(action === 'comment'){
+
     await MYKVSTORE.put('counter',value);
     return new Response(value);
+}
 }
