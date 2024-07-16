44fa524c33c02f4bd1a710ea631c665d6c0aa1b3
Vanshika <guptavanshika220@gmail.com>
2024-07-16 13:51:14 +0530

    update code

--- functions/combinefile.js
+++ functions/combinefile.js
@@ -10,17 +10,12 @@ export async function onRequest(context) {
         counterValue += 1;
     } else if(action === 'decrement'){
         counterValue -= 1;
-        
-    }else if(action ==='half'){
+    } else if(action ==='half'){
         counterValue = counterValue/2;
-      
-    }else if (action ==='double'){
+    } else if (action ==='double'){
         counterValue = counterValue*2;
-       
-    }
-    else if(action === 'reset'){
+    } else if(action === 'reset'){
         counterValue = 0;
-        
     } else if (action == 'getvalue') {
         return new Response(counterValue);
     }
