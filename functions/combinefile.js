a3f45e867d592f5da978f12efbb6430238c472f3
Vanshika <guptavanshika220@gmail.com>
2024-07-16 13:05:46 +0530

    deletion and modification

--- functions/combinefile.js
+++ functions/combinefile.js
@@ -8,22 +8,21 @@ export async function onRequest(context) {
 
     if (action === 'increment') {
         counterValue += 1;
-        await MYKVSTORE.put('counter', counterValue);
     } else if(action === 'decrement'){
         counterValue -= 1;
-        await MYKVSTORE.put('counter', counterValue);   
+        
     }else if(action === 'half'){
         counterValue = counterValue/2;
-        await MYKVSTORE.put('counter', counterValue); 
+      
     }else if (action === 'double'){
         counterValue = counterValue*2;
-        await MYKVSTORE.put('counter', counterValue); 
+       
     }
     else{
         counterValue === 0;
-        await MYKVSTORE.put('counter',counterValue);
+        
     }
 
-    
+    await MYKVSTORE.put('counter',counterValue);
     return new Response(counterValue);
 }
