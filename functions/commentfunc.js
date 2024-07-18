55414260e1d1ec5ece0c89aa0b9aa5ca077b638a
Vanshika <guptavanshika220@gmail.com>
2024-07-18 12:44:35 +0530

    remove of apos

--- functions/commentfunc.js
+++ functions/commentfunc.js
@@ -10,7 +10,7 @@ export async function onRequest(context) {
         try {
             
             // Store the message in KV store with a unique key
-            const messageKey = 'message';
+            const messageKey = message;
             console.log("line 7")
             await MYKVSTORE.put(messageKey, 'message');
             console.log("line 8")
